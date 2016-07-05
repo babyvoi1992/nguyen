b1a2edb239ec43a5ae833ca7e8842f410c7a53d5
Nguyen <baby.voi1992@gmail.com>
2016-07-05 22:23:34 +0700

    login app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,10 +14,24 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host= 'http://localhost:3000/'
-  config.action_mailer.default_url_options={host: host}
+  #config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.delivery_method = :test
+  #host= 'http://localhost:3000/'
+  #config.action_mailer.default_url_options={host: host}
+  config.action_mailer.raise_delivery_errors =true
+  config.action_mailer.delivery_method = :smtp
+  host = 'young-lake-14112.herokuapp.com'
+  config.action_mailer.default_url_options = {host: host}
+  ActionMailer::Base.smtp_settings ={
+      :address            => 'smtp.sendgrid.net',
+      :port               => '587',
+      :authentication     => :plain,
+      :user_name          =>  "app50481717@heroku.com",
+      :password           =>  "ykzhagok6341",
+      :domain             => 'heroku.com',
+      :enable_starttls_auto => true
+
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
